4a980c03b2a85a802e6f6347ad4087b4f32c80ac
Wangatracker <164025430+Wangatracker@users.noreply.github.com>
2025-02-22 14:51:04 +0300

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    botName: "NovemX Prime",
+    ownerName: "Elijah Tracker Wanga",
+    ownerNumber: "254758476795",
+    prefix: "[.]", // Bot command prefix
+    autoReadMessages: true,
+    autoTyping: true,
+    autoReadStatus: true
+};￼Enter
